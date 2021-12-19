e6e10dd0a40094731409204c086ab023177e610b
Max Teclav <maks.tetslav@yandex.ru>
2021-12-19 03:26:25 +0300

    feat: implement adding to favorites

--- src/scripts/controller/renderToys.ts
+++ src/scripts/controller/renderToys.ts
@@ -10,6 +10,30 @@ function renderToys(array: ToyList, target: Element) {
 
     return toy;
   });
+
+  const toys = document.querySelectorAll('.toy')!;
+  const favourites = document.querySelector('.favourites-text')!;
+
+  toys.forEach((toy) => {
+    toy.addEventListener('click', () => {
+      if (Number(favourites.innerHTML) === 20) {
+        if (toy.classList.contains('toy_favorite')) {
+          toy.classList.remove('toy_favorite');
+          favourites.innerHTML = String(Number(favourites.innerHTML) - 1);
+        } else {
+          alert('Извините, все слоты заполнены');
+        }
+      } else if (Number(favourites.innerHTML) < 20) {
+        if (!toy.classList.contains('toy_favorite')) {
+          toy.classList.add('toy_favorite');
+          favourites.innerHTML = String(Number(favourites.innerHTML) + 1);
+        } else {
+          toy.classList.remove('toy_favorite');
+          favourites.innerHTML = String(Number(favourites.innerHTML) - 1);
+        }
+      }
+    });
+  });
 }
 
 export default renderToys;
