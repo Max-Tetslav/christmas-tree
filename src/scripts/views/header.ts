ed4e095e227ffaf8e3fa3de1728670e492e885bd
Max Teclav <maks.tetslav@yandex.ru>
2021-12-27 21:10:12 +0300

    fix: fix routing for gh-pages

--- src/scripts/views/header.ts
+++ src/scripts/views/header.ts
@@ -5,15 +5,15 @@ const Header = {
       <nav class="menu">
         <ul class="menu__list">
           <li class="menu__list__item">
-            <a href="/#/" class="menu__list__item__link">
+            <a href="/#/" class="menu__list__item__link" id="home-view">
               <img class="menu__list__item__link__img" src="./assets/svg/homepage-logo.svg" alt="homepage-logo">
             </a>
           </li>
           <li class="menu__list__item">
-            <a href="/#/toys" class="menu__list__item__link">Игрушки</a>
+            <a href="/#/toys" class="menu__list__item__link" id="toys-view">Игрушки</a>
           </li>
           <li class="menu__list__item">
-            <a href="/#/tree" class="menu__list__item__link">Ёлка</a>
+            <a href="/#/tree" class="menu__list__item__link" id="tree-view">Ёлка</a>
           </li>
         </ul>
       </nav>
@@ -22,7 +22,25 @@ const Header = {
 
     return view;
   },
-  after_render: async () => {},
+  afterRender: async () => {
+    const homeLink: HTMLElement = document.querySelector('#home-view')! as HTMLElement;
+
+    homeLink.addEventListener('click', () => {
+      window.location.hash = '/';
+    });
+
+    const toysLink: HTMLElement = document.querySelector('#toys-view')! as HTMLElement;
+
+    toysLink.addEventListener('click', () => {
+      window.location.hash = '/toys';
+    });
+
+    const treeLink: HTMLElement = document.querySelector('#tree-view')! as HTMLElement;
+
+    treeLink.addEventListener('click', () => {
+      window.location.hash = '/tree';
+    });
+  },
 };
 
 export default Header;
